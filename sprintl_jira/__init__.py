c05e9976626774db3a52517c40d252991c6faf30
alex <alex@pdp7.net>
2019-05-15 04:13:57 +0200

    Add JIRA state extraction

--- sprintl_jira/__init__.py
+++ sprintl_jira/__init__.py
@@ -1,9 +1,18 @@
 import argparse
 import csv
+import datetime
 import getpass
 import os
 
 from atlassian import jira
+
+
+def get_issue(j, key):
+    return j.get('rest/api/2/issue/{0}?expand=changelog'.format(key))
+
+
+def convert_date(date):
+    return datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%f%z')
 
 
 def extract(url, username, password, sprint, output_dir):
@@ -12,13 +21,41 @@ def extract(url, username, password, sprint, output_dir):
     assert issues_jql
     os.makedirs(output_dir, exist_ok=True)
     with open(os.path.join(output_dir, 'issues.csv'), 'w') as issues_file:
-        issues_csv = csv.DictWriter(issues_file, ['id', 'summary', 'type'])
-        for issue in issues_jql['issues']:
-            issues_csv.writerow({
-                'id': issue['key'],
-                'summary': issue['fields']['summary'],
-                'type': issue['fields']['issuetype']['name'],
-            })
+        with open(os.path.join(output_dir, 'issue_states.csv'), 'w') as issue_states_file:
+            issues_csv = csv.DictWriter(issues_file, ['id', 'summary', 'type'])
+            issues_csv.writeheader()
+            issue_states_csv = csv.DictWriter(issue_states_file, ['issue', 'state', 'from', 'to'])
+            issue_states_csv.writeheader()
+            for issue in issues_jql['issues']:
+                key = issue['key']
+                issues_csv.writerow({
+                    'id': key,
+                    'summary': issue['fields']['summary'],
+                    'type': issue['fields']['issuetype']['name'],
+                })
+                issue_full = get_issue(j, key)
+                previous_time = convert_date(issue['fields']['created'])
+                histories = issue_full['changelog']['histories']
+                for history in histories:
+                    for item in history['items']:
+                        if not item['field'] == 'status':
+                            continue
+                        previous_state = item['fromString']
+                        last_state = item['toString']
+                        last_time = convert_date(history['created'])
+                        issue_states_csv.writerow({
+                            'issue': key,
+                            'state': previous_state,
+                            'from': previous_time,
+                            'to': last_time,
+                        })
+                        previous_time = last_time
+                issue_states_csv.writerow({
+                    'issue': key,
+                    'state': last_state,
+                    'from': last_time,
+                    'to': None,
+                })
 
 
 def main():
